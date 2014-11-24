8c920a2b6c582ad0d83ac324d809bfb0ae3525d2
Bartosz Nowak <sztosz@gmail.com>
2014-11-24 21:41:55 +0100

    Basic functionality, can play, stop, pause and change file.

--- src/FXPlayer/Controller.java
+++ src/FXPlayer/Controller.java
@@ -2,6 +2,8 @@ package FXPlayer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -11,31 +13,54 @@ public class Controller {
 
     final FileChooser fileChooser = new FileChooser();
     private Stage stage;
+    private Media media;
+    private MediaPlayer mediaPlayer;
 
     @FXML
     private Label songTicker;
 
     @FXML
-    void changeSong(){
-        File file = fileChooser.showOpenDialog(stage);
-        if (file != null) {
-            songTicker.setText(file.toURI().toString());
+    void changeSong() {
+        try {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                final Media media = new Media(file.toURI().toString());
+                final MediaPlayer mediaPlayer = new MediaPlayer(media);
+                mediaPlayer.setOnError(new Runnable() {
+                    @Override
+                    public void run() {
+                        songTicker.setText(media.getError().getMessage());
+                    }
+                });
+                songTicker.setText("Now Playing"); // TO DO get song name
+                if (this.mediaPlayer != null) {
+                    this.mediaPlayer.stop();
+                }
+                mediaPlayer.play();
+                this.mediaPlayer = mediaPlayer;
+                this.media = media;
+            }
+        } catch (RuntimeException re) {
+            songTicker.setText("Can't play this file");
         }
     }
 
     public void playClicked() {
+        mediaPlayer.play();
         songTicker.setText("PLAY");
     }
 
     public void pauseClicked() {
+        mediaPlayer.pause();
         songTicker.setText("PAUSE");
     }
 
     public void stopClicked() {
+        mediaPlayer.stop();
         songTicker.setText("STOP");
     }
 
-    public void setStage(Stage stage){
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
 }
